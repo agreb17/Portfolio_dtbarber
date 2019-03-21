c3a0e495b6e95bf471e7ac89a491b722710b805a
Anthony Greb <agreb255@gmail.com>
2019-03-21 16:28:38 -0400

    some basic setup

--- dtbarber1/src/App.js
+++ dtbarber1/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+// import logo from './logo.svg';
 import styled from "styled-components";
 import { Route } from "react-router-dom";
+import Home from "./views/Home";
 import './App.css';
 
 
-
 const AppContainer = styled.div`
-width: ;
-height: ;
+width: 100%;
+height: 100%vh;
 `;
 class App extends Component {
   render() {
     return (
       <AppContainer>
-          {/* <Route exact path="/" render={  } /> */}
-      </AppContainer>
+          <Route exact path="/" render={() => <Home /> } />
+        </AppContainer>
+      
     );
   }
 }
---
+++ dtbarber1/src/views/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+import styled from "styled-components";
+
+const AboutDiv = styled.div`
+width: 100%;
+height: 100vh;
+`;
+
+
+const About = () => {
+    return (
+        <AboutDiv>
+
+        </AboutDiv>
+    )
+
+}
+
+
+export default About;
