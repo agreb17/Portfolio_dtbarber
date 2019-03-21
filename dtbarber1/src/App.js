b1c89d9942d82f89470f4ae8437ada38e0372919
Anthony Greb <agreb255@gmail.com>
2019-03-21 14:14:15 -0400

    modifying app.js

--- dtbarber1/src/App.js
+++ dtbarber1/src/App.js
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import styled from "styled-components";
+import { Route } from "react-router-dom";
 import './App.css';
 
+
+
+const AppContainer = styled.div`
+width: ;
+height: ;
+`;
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <AppContainer>
+          {/* <Route exact path="/" render={  } /> */}
+      </AppContainer>
     );
   }
 }
