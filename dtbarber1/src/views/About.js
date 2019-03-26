027426cb3f9534618157c212315225fea009b3e7
Anthony Greb <agreb255@gmail.com>
2019-03-26 15:12:06 -0400

    added images for products.js

--- dtbarber1/src/views/About.js
+++ dtbarber1/src/views/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 
+
 const AboutDiv = styled.div`
 width: 100%;
 height: 100vh;
@@ -10,6 +11,8 @@ const HeaderDiv = styled.div`
 display: flex;
 justify-content: center;
 `;
+
+
 
 const Header = styled.h1`
 height: min-content;
